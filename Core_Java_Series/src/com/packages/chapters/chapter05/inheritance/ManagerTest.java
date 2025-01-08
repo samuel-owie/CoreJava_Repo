da88304aca26a32924e53fb374ebdd69214f55cc
samuel-owie <owiesamuelehizogie@gmail.com>
2025-01-08 15:38:44 -0500

    "Package =inheritance class creation"

--- Core_Java_Series/src/com/packages/chapters/chapter05/inheritance/ManagerTest.java
+++ Core_Java_Series/src/com/packages/chapters/chapter05/inheritance/ManagerTest.java
@@ -1,4 +1,13 @@
 package com.packages.chapters.chapter05.inheritance;
 
+/**
+ * This program demonstrates inheritance
+ * @version 1.0 09.01.2025 (dd-mm-yyyy)
+ * @author Samuel Owie
+*/
+
 public class ManagerTest {
+    public static void main(String[] args){
+        //
+    }
 }
