c6b409b8ce43f98da467239a7056121baa7d33ee
samuel-owie <owiesamuelehizogie@gmail.com>
2025-01-08 15:55:43 -0500

    "Package =inheritance class modification with needed methods"

--- Core_Java_Series/src/com/packages/chapters/chapter05/inheritance/Employee.java
+++ Core_Java_Series/src/com/packages/chapters/chapter05/inheritance/Employee.java
@@ -1,4 +1,34 @@
 package com.packages.chapters.chapter05.inheritance;
 
+import java.time.*;
+
 public class Employee {
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    // Constructor
+    public Employee(String name, double salary, int year, int month, int day){
+        this.name = name;
+        this.salary = salary;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    // Getters and setters
+    public String getName(){
+        return name;
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+
+    public LocalDate getHireDay(){
+        return hireDay;
+    }
+
+    public void raiseSalary(double byPercent){
+        double raise= salary * byPercent / 100;
+        salary += raise;
+    }
 }
--- Core_Java_Series/src/com/packages/chapters/chapter05/inheritance/ManagerTest.java
+++ Core_Java_Series/src/com/packages/chapters/chapter05/inheritance/ManagerTest.java
@@ -8,6 +8,21 @@ package com.packages.chapters.chapter05.inheritance;
 
 public class ManagerTest {
     public static void main(String[] args){
-        //
+        // Construct a Manager object
+        var boss = new Manager("Carl", 80000, 1987, 12, 15);
+        boss.setBonus(5000);
+
+        var staff = new Employee[3];
+
+        // Fill the staff array with Manager and Employee objects
+
+        staff[0] = boss;
+        staff[1] = new Employee("Harry", 50000, 1989, 10, 1);
+        staff[2] = new Employee("Tommy", 40000, 1990, 3, 15);
+
+        // Print out information about all Employee Objects
+        for(Employee e : staff){
+            System.out.println("name= " + e.getName() + ", salary= " + e.getSalary());
+        }
     }
 }
