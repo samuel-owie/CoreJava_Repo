978a5f2edab8d5516aa48c08475323eefe428d80
samuel-owie <owiesamuelehizogie@gmail.com>
2025-01-10 14:03:54 -0500

    -Employee, EqualsTest & Manager class setup

--- Core_Java_Series/src/com/packages/chapters/chapter05/equals/Manager.java
+++ Core_Java_Series/src/com/packages/chapters/chapter05/equals/Manager.java
@@ -1,4 +1,21 @@
 package com.packages.chapters.chapter05.equals;
 
-public class Manager {
+public class Manager extends Employee {
+
+    private double bonus;
+
+    //Constructor
+    public Manager(String name, double salary, int year, int month, int day){
+        super(name, salary, year, month, day);
+        bonus = 0;
+    }
+
+    public double getSalary(){
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+
+    public void setBonus(double bonus){
+        this.bonus = bonus;
+    }
 }
