5d0eaf6e5b6bb82033c0eb7dd5e819faf055cfe4
samuel-owie <owiesamuelehizogie@gmail.com>
2025-01-10 13:11:20 -0500

    Modifications to the ManagerTest class

--- Core_Java_Series/src/com/packages/chapters/chapter05/inheritance/ManagerTest.java
+++ Core_Java_Series/src/com/packages/chapters/chapter05/inheritance/ManagerTest.java
@@ -31,6 +31,6 @@ public class ManagerTest {
             //System.out.println(executiveBoss.getSAwards());
         }
 
-        System.out.println(System.out);
+        //System.out.println(System.out);
     }
 }
