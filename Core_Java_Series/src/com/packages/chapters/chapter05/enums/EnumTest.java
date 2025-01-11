37dcc220be862aa1547ef2dfdbe443b14d28b1d6
samuel-owie <owiesamuelehizogie@gmail.com>
2025-01-11 12:26:15 -0500

    -Enum package setup

---
+++ Core_Java_Series/src/com/packages/chapters/chapter05/enums/EnumTest.java
@@ -0,0 +1,18 @@
+package com.packages.chapters.chapter05.enums;
+
+import java.util.*;
+/**
+ * This program demonstrates enumerated classes.
+ * @version 1.0 11.01.2025
+ * @author Samuel Owie
+ */
+
+public class EnumTest {
+    public static void main(String[] args){
+        var in = new Scanner(System.in);
+
+        System.out.print("Enter a size: (SMALL, MEDIUM, LARGE, EXTRA_LARGE) ");
+
+        String input= in.next().toUpperCase();
+    }
+}
