5f01ef2a1c7436276b992b1dc5aa659c2edd9331
samuel-owie <owiesamuelehizogie@gmail.com>
2025-02-01 18:04:28 +0100

    -weather data setup

--- Core_Java_Series/src/com/packages/chapters/chapter06/interfaces/EmployeeSortTest.java
+++ Core_Java_Series/src/com/packages/chapters/chapter06/interfaces/EmployeeSortTest.java
@@ -13,7 +13,7 @@ public class EmployeeSortTest {
 
         staff[0]= new Employee("Harry", 35000);
         staff[1]= new Employee("Carl", 75000);
-        staff[2]= new Employee("Tony", 38000);
+        staff[2]= new Employee("Tony", 38000); //
 
         Arrays.sort(staff);
 
