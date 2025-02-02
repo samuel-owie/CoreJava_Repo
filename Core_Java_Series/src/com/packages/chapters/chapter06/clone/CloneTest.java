e8d67a39811549c65674a0b006d19209b7a6d89a
samuel-owie <owiesamuelehizogie@gmail.com>
2025-02-02 19:17:20 +0100

    -Clone implementation

---
+++ Core_Java_Series/src/com/packages/chapters/chapter06/clone/CloneTest.java
@@ -0,0 +1,14 @@
+package com.packages.chapters.chapter06.clone;
+
+/**
+ * This program demonstrates enumerated classes.
+ * @version 1.0 02.02.2025
+ * @author Samuel Owie
+ */
+
+public class CloneTest {
+
+    public static void main(String[] args) throws CloneNotSupportedException{
+        var original = new Employee("John Q. Public", 5000);
+    }
+}
---
+++ Core_Java_Series/src/com/packages/chapters/chapter06/clone/Employee.java
@@ -0,0 +1,9 @@
+package com.packages.chapters.chapter06.clone;
+
+import java.util.*;
+
+public class Employee implements Cloneable{
+    private String name;
+    private double salary;
+    private Date hireDate;
+}
