b55266c2019359a5a0f178d30f9d08fb5d079cf7
samuel-owie <owiesamuelehizogie@gmail.com>
2025-02-02 19:23:59 +0100

    -Clone implementation

--- Core_Java_Series/src/com/packages/chapters/chapter06/clone/Employee.java
+++ Core_Java_Series/src/com/packages/chapters/chapter06/clone/Employee.java
@@ -1,9 +1,25 @@
 package com.packages.chapters.chapter06.clone;
 
 import java.util.*;
+import java.time.*;
 
 public class Employee implements Cloneable{
     private String name;
     private double salary;
-    private Date hireDate;
+    private Date hireDay;
+
+    public Employee(String name, double salary){
+        this.name = name;
+        this.salary = salary;
+        hireDay = new Date();
+    }
+    public Employee clone() throws CloneNotSupportedException{
+        //call Object.clone
+        Employee cloned = (Employee) super.clone();
+
+        //clone mutable fields
+        cloned.hireDay = (Date) hireDay.clone();
+
+        return cloned;
+        }
 }
